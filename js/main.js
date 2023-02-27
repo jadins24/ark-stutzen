8da81d13e6871438d21d02f33ce61f32d387f5be
jadins24 <jadinesh04@gmail.com>
2023-02-27 23:40:06 +0530

    add

--- js/main.js
+++ js/main.js
@@ -12,15 +12,3 @@ $('.i-slide').owlCarousel({
 		
 	}
 });
-var i=1;
-		function fun(){
-			i++;
-			if(i%2==0){
-				document.querySelector(".nav").style="display:flex";
-				document.querySelector(".menu-bar").style="background-color:#414252;padding: 20px;";
-			}
-			if(i%2!=0){
-				document.querySelector(".nav").style="display:none";
-				document.querySelector(".menu-bar").style="background-color:none;padding: 0px;";				
-			}
-		}
