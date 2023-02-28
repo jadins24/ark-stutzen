2e95fc6bd35cb7e014df631574f4d11a4076f108
jadins24 <jadinesh04@gmail.com>
2023-02-28 22:27:36 +0530

    add

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-$('.i-slide').owlCarousel({
-    autoplay:true,
-    loop:true,
-	items:2,
-	center:true,
-    margin:20,
-    responsive:{
-		
-		600:{
-			items:5
+var i=1;
+		function fun(){
+			i++;
+			if(i%2==0){
+				document.querySelector(".nav").style="display:flex";
+				document.querySelector(".menu-bar").style="background-color:#414252;padding: 20px;";
+			}
+			if(i%2!=0){
+				document.querySelector(".nav").style="display:none";
+				document.querySelector(".menu-bar").style="background-color:none;padding: 0px;";				
+			}
 		}
-		
-	}
-});
